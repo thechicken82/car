489bce7c7982f629087a47d1aee02aa51180c5a0
thechicken82 <ethan.nadeau@outlook.com>
2024-11-24 22:22:44 -0600

    commit

--- car.go
+++ car.go
@@ -1,9 +1,19 @@
 package car
 
+import (
+	"fmt"
+
+	"github.com/thechicken82/car_base"
+)
+
 func Honk() string {
 	return "Honk..."
 }
 
 func Honks() string {
 	return "Honks..."
+}
+
+func The_Car_Base() {
+	fmt.Println(car_base.Car_Base())
 }
